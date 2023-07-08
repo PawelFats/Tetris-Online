fd2144e00876f7be570fef9501af5c9002a83703
pavel.fatyanov <pawelfats@gmail.com>
2023-07-08 23:09:28 +0700

    Added ncurses for graphics

--- src/main.c
+++ src/main.c
@@ -12,10 +12,10 @@ void printTetGame(TetGame* tetg) {
     {
         for(int j=0; j<tf->width; j++)
         {
-            int sym = 0;
+            int sym = 1;
             if(tf->blocks[i*tf->width+j].b != 0)
             {
-                sym = 1;
+                sym = 2;
             }
             else
             {
@@ -24,18 +24,28 @@ void printTetGame(TetGame* tetg) {
                 if(x >= 0 && x < t->size && y >= 0 && y < t->size)
                 {
                     if(t->blocks[y*t->size+x].b != 0)
-                        sym = 1;
+                        sym = 2;
                 }
-                printf("%d", sym);
+                attron(COLOR_PAIR(sym));
+                mvaddch(i, j, ' ');
+                attroff(COLOR_PAIR(sym));
             }
         }
     }
-    fflush(stdout);
 }
 
 int main(int argc, char* argv[]) {
 
     struct timespec sp_start, sp_end, ts1, ts2 = {0, 0};
+
+    initscr();
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_MAGENTA);
+    init_pair(2, COLOR_GREEN, COLOR_GREEN);
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+    scrollok(stdscr, TRUE);
 
     TetGame* tetg = createTetGame(35, 31, 5,
     7, tet_templates);
@@ -47,14 +57,30 @@ int main(int argc, char* argv[]) {
 
     while(tetg->playing != TET_GAMEOVER)
     {
-        //initscr();
-
         clock_gettime(CLOCK_MONOTONIC, &sp_start);
+
+        int ch = getch();
+        switch (ch) {
+            case 'w':
+                player.action = TET_PLAYER_UP;
+                break;
+            case 's':
+                player.action = TET_PLAYER_DOWN;
+                break;
+            case 'a':
+                player.action = TET_PLAYER_LEFT;
+                break;
+            case 'd':
+                player.action = TET_PLAYER_RIGHT;
+                break;
+            default:
+                player.action = TET_PLAYER_NOP;
+                break;
+        }
+
         calculateTet(tetg);
-
-        //refresh();
-
         printTetGame(tetg);
+
         clock_gettime(CLOCK_MONOTONIC, &sp_end);
         if(sp_end.tv_sec - sp_start.tv_sec <= 0 && (ts2.tv_nsec = 33000000 -
                 (sp_end.tv_nsec - sp_start.tv_nsec)) > 0)
@@ -62,11 +88,8 @@ int main(int argc, char* argv[]) {
             nanosleep(&ts2, &ts1);
         }
 
-        //refresh();
-        //getch();
-        //endwin();
     }
-
-    printf("Hello, World!\n");
+    freeTetGame(tetg);
+    endwin();
     return 0;
 }
